61faaa74393ebb5c80e1f1fd2dd69b0cbc47bc41
Hoang Nam <namhoang.td09@gmail.com>
2016-06-06 20:51:54 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// 3RES Note Sample App
+// 3RES Messager Sample App
 // index.js
 
 // Express
-
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var path = require('path');
 
 // Socket.io
-
+var io = require('socket.io')(server);
 
 // Rethinkdb
-
+var r = require('rethinkdb');
 
 // Socket.io changefeed events
-
+var changefeedSocketEvents = require('./socket-events.js');
